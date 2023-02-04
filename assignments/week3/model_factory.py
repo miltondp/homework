b5e94d2423c8ec279488b8b7e6aad19c81b2c2df
Milton Pividori <miltondp@gmail.com>
2023-02-04 16:36:51 -0500

    model_factory: try simpler model

--- assignments/week3/model_factory.py
+++ assignments/week3/model_factory.py
@@ -16,7 +16,7 @@ def create_model(input_dim: int, output_dim: int) -> MLP:
     """
     return MLP(
         input_dim,
-        [1024, 32, 256, 512, 1024],
+        [256],
         output_dim,
         torch.nn.ReLU,
         torch.nn.init.xavier_uniform_,
