91c1e497d86a0aed39192d20ebae1dcb84b6f53e
Milton Pividori <miltondp@gmail.com>
2023-02-06 09:04:30 -0500

    model_factory: update hyperparameters

--- assignments/week3/model_factory.py
+++ assignments/week3/model_factory.py
@@ -16,8 +16,8 @@ def create_model(input_dim: int, output_dim: int) -> MLP:
     """
     return MLP(
         input_dim,
-        [1024, 1024, 128],
+        [1024, 1024, 256],
         output_dim,
-        torch.nn.ReLU,
+        torch.nn.LeakyReLU,
         torch.nn.init.xavier_normal_,
     )
