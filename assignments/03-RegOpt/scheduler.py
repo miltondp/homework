2af15ae80e99c8dd58e5b25ff0587ab9277e039f
Milton Pividori <miltondp@gmail.com>
2023-02-17 11:58:33 -0500

    scheduler: update lr once per epoch

--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -16,16 +16,23 @@ class CustomLRScheduler(_LRScheduler):
         if you need to add new parameters.
 
         """
-        # ... Your Code Here ...
+        self.gamma = 0.65
+        self.step_size = 781.0
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
 
     def get_lr(self) -> List[float]:
         """
         Return the current learning rate for each parameter group.
         """
-        # Note to students: You CANNOT change the arguments or return type of
-        # this function (because it is called internally by Torch)
 
-        # ... Your Code Here ...
-        # Here's our dumb baseline implementation:
-        return [i for i in self.base_lrs]
+        # update only once per epoch, not per batch
+        if (self.last_epoch == 0) or (self.last_epoch % self.step_size != 0):
+            return [group["lr"] for group in self.optimizer.param_groups]
+
+        current_epoch = (self.last_epoch / self.step_size) - 1
+
+        vals = [
+            group["lr"] * (self.gamma**current_epoch)
+            for group in self.optimizer.param_groups
+        ]
+        return vals
