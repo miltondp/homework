7577c919d5ab13488a089a799bc68be6b749fe3f
Milton Pividori <miltondp@gmail.com>
2023-02-17 12:25:46 -0500

    config: adjust initial lr

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -8,7 +8,7 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 class CONFIG:
     batch_size = 64
     num_epochs = 10
-    initial_learning_rate = 0.001
+    initial_learning_rate = 0.002
     initial_weight_decay = 0
 
     lrs_kwargs = {
