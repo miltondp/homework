99161f1753a4d91245da5062b7810b6112125dc6
Milton Pividori <miltondp@gmail.com>
2023-02-18 00:46:48 -0500

    change normalize transform

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -29,8 +29,8 @@ class CONFIG:
         [
             ToTensor(),
             Normalize(
-                mean=[0.5, 0.5, 0.5],
-                std=[0.2, 0.2, 0.2],
+                mean=[0.485, 0.456, 0.4],
+                std=[0.229, 0.224, 0.2],
             ),
         ]
     )
