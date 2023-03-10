3fba7d664c85720b28a44df1844d6da2019a75e2
Milton Pividori <miltondp@gmail.com>
2023-03-10 16:47:21 -0500

    implement initial model

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -1,9 +1,33 @@
 import torch
+from torch import nn
+import torch.nn.functional as F
 
 
 class Model(torch.nn.Module):
+    """
+    A simple CNN.
+    """
+
     def __init__(self, num_channels: int, num_classes: int) -> None:
-        ...
+        super(Model, self).__init__()
+        self.conv1 = nn.Conv2d(num_channels, 6, 5)
+        # self.pool = nn.MaxPool2d(2, 2)
+        # self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(
+            self.conv1.out_channels
+            * (32 - self.conv1.kernel_size[0] + 1)
+            * (32 - self.conv1.kernel_size[1] + 1),
+            120,
+        )
+        self.fc2 = nn.Linear(120, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        ...
+        """
+        Forward pass of the model.
+        """
+        x = F.relu(self.conv1(x))
+
+        x = x.view(-1, self.fc1.in_features)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
