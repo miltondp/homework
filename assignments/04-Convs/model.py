7dd3d1e36fc59d8ee47bacbd48ff82e44d7c0742
Milton Pividori <miltondp@gmail.com>
2023-03-10 17:10:58 -0500

    model: add layers

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -11,12 +11,10 @@ class Model(torch.nn.Module):
     def __init__(self, num_channels: int, num_classes: int) -> None:
         super(Model, self).__init__()
         self.conv1 = nn.Conv2d(num_channels, 6, 5)
-        # self.pool = nn.MaxPool2d(2, 2)
-        # self.conv2 = nn.Conv2d(6, 16, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(
-            self.conv1.out_channels
-            * (32 - self.conv1.kernel_size[0] + 1)
-            * (32 - self.conv1.kernel_size[1] + 1),
+            self.conv2.out_channels * 5 * 5,
             120,
         )
         self.fc2 = nn.Linear(120, num_classes)
@@ -25,7 +23,8 @@ class Model(torch.nn.Module):
         """
         Forward pass of the model.
         """
-        x = F.relu(self.conv1(x))
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
 
         x = x.view(-1, self.fc1.in_features)
         x = F.relu(self.fc1(x))
